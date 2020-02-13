5139e42971b1be0c02c5aee3aee2040f4e91de47
Pedro-H-Castoldi <pedrohenriquecastoldi.b@hotmail.com>
2020-02-12 21:31:56 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+//1) a)
 document.getElementById("primeiro").style= "color: blue"
+
+// b)
+
+
+//c)
+document.getElementById("quinto").parentNode.style= "border: dashed 1px red"
+
+//d)
+const pai = document.getElementById("quinto").parentNode;
+console.log(pai.previousElementSibling.style= "color: red")
